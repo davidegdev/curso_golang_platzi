d9b982de1aaa80da3cba028163578e8aaed68ad7
davidegdev <escorciagomezdavid@gmail.com>
2025-03-30 15:52:48 -0500

    Replace main.go with clase_11.go implementing various functions and examples

---
+++ src/clase_11.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func normalFunction(message string) {
+	fmt.Println(message)
+}
+
+func tripleArgument(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func returnValuePerTwo(a int) int{
+	return a * 2
+}
+
+func doubleReturn(a int) (b, c int){
+	return a, a*2
+}
+
+func main() {
+	normalFunction("Hola mundo")
+	tripleArgument(1, 2, "Hola")
+	value := returnValuePerTwo(3)
+	fmt.Println("Value: ", value)
+	value1, value2 := doubleReturn(3)
+	fmt.Println("Value1 & value2: ", value1, value2)
+
+	// Usar solamente una de las variables del return de una funcion
+	_, value3 := doubleReturn(4)
+	fmt.Println("Value3:", value3)
+}
