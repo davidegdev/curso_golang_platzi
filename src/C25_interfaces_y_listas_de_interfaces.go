6f3b3369bb906223738b3674a0b7c15e9b1290ae
davidegdev <escorciagomezdavid@gmail.com>
2025-06-09 20:23:49 -0500

    Refactor C25_interfaces_y_listas_de_interfaces.go to add rectangulo struct and update area calculations

--- src/C25_interfaces_y_listas_de_interfaces.go
+++ src/C25_interfaces_y_listas_de_interfaces.go
@@ -1,13 +1,41 @@
 package main
 
+import "fmt"
+
+
+type figuras2D interface{
+	area() float64
+}
+
 type cuadrado struct {
 	base float64
+}
+
+type rectangulo struct {
+	base float64
+	altura float64
 }
 
 func (c cuadrado) area() float64{
 	return c.base * c.base
 }
 
+func (r rectangulo) area() float64{
+	return r.altura * r.base
+}
+
+func calcularArea(f figuras2D){
+	fmt.Println("Area:", f.area())
+}
+
 func main() {
 	myCuadrado := cuadrado{base: 2}
+	myRectangulo := rectangulo{base: 2, altura: 4}
+
+	calcularArea(myCuadrado)
+	calcularArea(myRectangulo)
+
+	// Lista interfaces. Listas multitipo o de multiples tipos.
+	myInterface := []interface{}{"Hola", 12, 4.25}
+	fmt.Println(myInterface...)
 }
