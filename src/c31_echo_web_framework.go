044346c05ce6beb6d2b9744d34892149a3e46004
davidegdev <escorciagomezdavid@gmail.com>
2025-06-28 19:17:57 -0500

    Add main.go to implement basic structure for Echo web framework

--- src/c31_echo_web_framework.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main(){
-
-}
-
-// COMANDOS
-
-// go install -v -u github.com/labstack/echo/v4@latest
---
+++ src/c31_web/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main(){
+
+}
+
+// COMANDOS
+
+// go install -v github.com/labstack/echo/v4@latest or with flag -u if already installed
