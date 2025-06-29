abaab20273cbff7e42099f707b2aa0bc9574cefc
davidegdev <escorciagomezdavid@gmail.com>
2025-06-29 16:09:07 -0500

    Refactor main.go to improve structure and readability; reintroduce doubleReturn function and update variable initialization.

--- src/main.go
+++ src/main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
-	"github.com/labstack/echo"
+	"fmt"
+
+	//"github.com/labstack/echo"
 )
 
+func doubleReturn(a int) (c, d int) { return a, a * 2 };
+
 func main(){
+
+	var num uint8 = 200
 	
 	// Instance echo
-	e := echo.New()
+	/*e := echo.New()
 
 	// Create a new route
 	e.GET("/", func(c echo.Context) error {
 		return c.String(200, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323")) // Start the server
+	e.Logger.Fatal(e.Start(":1323")) // Start the server*/
+
+	fmt.Printf("2, %T", 2)
+	fmt.Println()
+	fmt.Println("num: ", num)
+	x,y := doubleReturn(2)
+
+	fmt.Println("Valor de x:", x)
+	fmt.Println("Valor de y:", y)
+	
 }
 
 // COMANDOS TERMINAL
